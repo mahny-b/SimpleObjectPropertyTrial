20d338f14cf1af18cc574b9df3dde324e65c7911
@mahny_b <mahny.brand@gmail.com>
2017-05-30 02:40:33 +0900

    プロジェクト構成完成

---
+++ src/main/java/jp/gr/java_conf/mahny/simple_object_property_trial/App.java
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.mahny.simple_object_property_trial;
+
+/**
+ * <p>こっちはダミー。テストコードを参照して下さい</p>
+ * @author mahny
+ */
+public class App
+{
+	/**
+	 * 起動用
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// nop
+	}
+}
---
+++ src/test/java/jp/gr/java_conf/mahny/simple_object_property_trial/SimpleObjectPropertyTrial.java
@@ -0,0 +1,15 @@
+package jp.gr.java_conf.mahny.simple_object_property_trial;
+
+/**
+ * 主にSimpleObjectProperty系統の使い方
+ * @author mahny
+ */
+public class SimpleObjectPropertyTrial {
+
+	/**
+	 *
+	 */
+	public void trySimpleStringProperty1() {
+
+	}
+}
